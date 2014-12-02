2ddd6ff8d1ef69852fbb45c9a1935febf284238c
Brady Olsen <bradyaolsen@gmail.com>
2014-12-01 23:46:56 -0800

    added template styles

---
+++ sample-twig/public/lib/menu/script.js
@@ -0,0 +1,5 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+});
+} )( jQuery );
