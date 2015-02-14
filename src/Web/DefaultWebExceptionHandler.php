316b597acb7486eda64a790982f77ff9c1dbf424
Brady Olsen <bradyaolsen@gmail.com>
2015-02-14 10:35:31 -0800

    make default exception handler not show trace for security reasons

--- src/Web/DefaultWebExceptionHandler.php
+++ src/Web/DefaultWebExceptionHandler.php
@@ -6,10 +6,19 @@ use Exception;
 
 class DefaultWebExceptionHandler implements WebExceptionHandler
 {
+    private $showTrace;
+
+    public function __construct($showTrace = false)
+    {
+        $this->showTrace = $showTrace;
+    }
+
     public function handle(Exception $exception, Request $request = null)
     {
-        $content = "Application error: " . $exception->getMessage() . "\n\n";
-        $content .= "Exception trace:\n" . ExceptionUtility::getFullTraceAsString($exception) . "\n";
-        return new TextResponse($content, 500, array());
+        $content = "Application error: " . $exception->getMessage() . "\n";
+        if ($this->showTrace) {
+            $content .= "\nException trace:\n" . ExceptionUtility::getFullTraceAsString($exception) . "\n";
+        }
+        return new TextResponse($content, 500);
     }
 }
